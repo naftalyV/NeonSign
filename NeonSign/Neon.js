bc9e13b57f1cf0c3fd79212254f54c23d76f8c58
906 <906@SHACHAM24>
2017-04-03 19:35:49 +0300

    Add project files.

---
+++ NeonSign/Neon.js
@@ -0,0 +1,31 @@
+﻿var canvas = document.getElementById("myCanvas");
+var stop = document.getElementById("Stop").addEventListener("click", stop);
+function changeColor() {
+
+}
+
+var myColor = ["red", "blue", "yello", "gold"];
+
+var currentIndex = 0;
+
+var intervalId = setInterval(start, 1000);
+
+function start() {
+
+    if (currentIndex === myColor.length) {
+
+        currentIndex = 0;
+
+    }
+
+    alert(myArr[currentIndex]);
+
+    currentIndex++;
+
+}
+
+function stop() {
+
+    clearInterval(intervalId);
+
+}
